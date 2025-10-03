c91b6fa9c2b4df2f6da2b75d92a920a761d8af1d
Ridz Coder <148397393+ridz-coder01@users.noreply.github.com>
2025-10-04 00:37:01 +0300

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-  email: "darkwebagent096@gmail.com",
-  password: "Darknetofficialgh@@2144",
+  email: "opakstg@gmail.com",
+  password: "sweetsixteen",
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
 };
